591e637674c339eea8d3bba89cfb682d76752188
Sergey <wiggo91@mail.ru>
2023-03-17 21:17:28 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0101;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Я думаю программистом быть круто");
+        System.out.println("Я думаю, быть программистом круто");
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -6,8 +6,9 @@ package com.javarush.task.task01.task0105;
 
 public class Solution {
     public static void main(String[] args) {
-       String name ;
-       int age;
-       String city;
+        String name;
+        int age;
+        String city;
+
     }
 }
