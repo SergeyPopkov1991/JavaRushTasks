ab68ca3cc104f1dabe750cdb417fab7c05ba5484
Sergey <wiggo91@mail.ru>
2023-03-18 19:13:37 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 0;
+        int numericValue = 1;
+        int size = 2;
+        int number = 3;
+
+    }
+}
