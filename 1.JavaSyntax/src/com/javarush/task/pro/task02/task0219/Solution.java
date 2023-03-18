dda325c1aff8c279bad71391deab6cee3011ebdc
Sergey <wiggo91@mail.ru>
2023-03-18 19:01:35 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0219;
+
+/* 
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       int id = 0 ;
+       int age = 1;
+       String documents = "left";
+       String behavior = "right";
+    }
+}
