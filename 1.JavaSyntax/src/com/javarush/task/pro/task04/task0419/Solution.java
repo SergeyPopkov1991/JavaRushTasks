1ea2f33f6a7604e5cca5b6dfc83f5b865ae9003d
Sergey <wiggo91@mail.ru>
2023-03-19 22:03:18 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0419/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0419;
+
+import java.util.Scanner;
+
+/* 
+Защита от землетрясения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        //напишите тут ваш код
+        System.out.println(sum);
+    }
+}
