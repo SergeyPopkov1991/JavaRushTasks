ca63ed105a2641383f3d3eda7c9b2b86a8fa334e
Sergey <wiggo91@mail.ru>
2023-03-28 22:33:49 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+       for (int i = 0 ; i >3 ; i++){
+
+       }
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        if (a.equalsIgnoreCase(secret)){
+            System.out.println("доступ разрешен");
+        }else {
+            System.out.println("доступ запрещен");
+        }
+
+
+    }
+}
