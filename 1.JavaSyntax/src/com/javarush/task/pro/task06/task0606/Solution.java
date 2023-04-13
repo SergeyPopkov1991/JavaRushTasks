4a3fe7458a7a8a12cebb3fe1be1ac5771c980107
Sergey <wiggo91@mail.ru>
2023-04-13 22:51:18 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(String name){
+
+    }
+
+    public static void universalMethod(int i){
+
+    }
+    public static void universalMethod(float i){
+
+    }
+    public static void universalMethod(String name , int i){
+
+    }
+    public static void universalMethod(String name , int i, float j){
+
+    }
+    public static void universalMethod(double i){
+
+    }
+    public static void universalMethod(char i){
+
+    }
+    public static void universalMethod(byte i){
+
+    }
+    public static void universalMethod(long i ){
+
+    }
+    public static void universalMethod(short i){
+
+    }
+}
