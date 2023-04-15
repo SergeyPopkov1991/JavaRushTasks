0085c0eb1da840c1ccf18915484ce9c4df405323
Sergey <wiggo91@mail.ru>
2023-04-15 23:08:47 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    long i ;
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long i) {
+        i = i * i * i;
+
+        return i;
+
+
+    }
+
+}
