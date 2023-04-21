0baf389463306abade417c897016f64e1f9971cb
Sergey <wiggo91@mail.ru>
2023-04-21 22:09:30 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0622/Solution.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 */
 
 public class Solution {
-
     public static String getFirstPositionPhrase = "Помоги Амиго определить, где спрятался Диабло? Введи номер позиции (1, 2, 3 или 4):";
     public static String getPositionPhrase = "Диабло не обнаружен. Давай попробуем снова. Введи номер позиции (1, 2, 3 или 4):";
     public static String findDiabloPhrase = "Амиго обнаружил Диабло.";
@@ -16,30 +15,49 @@ public class Solution {
     public static String winPhrase = "Амиго одержал победу над Диабло и выбил из его головы загадочный кристалл.";
     public static String loosePhrase = "Диабло победил Амиго.";
     public static int diabloPosition;
+    public static int  amigoLives = 9;
+    public static int diabloLives = 9;
 
-    public static void findDiablo() {
-        System.out.println(getFirstPositionPhrase);
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
+    public static void amigoLostLife(){
+        amigoLives =   amigoLives - 1;
+
+    }
+
+    public static void diabloLostLife(){
+        diabloLives = diabloLives - 3;
+
+    }
+
+    public static int amigoAttacks(){
+
+        return getRandomNumber(3);
 
 
-        int a = scanner.nextInt();
-
-
-        if (a == diabloPosition) {
-            System.out.println(findDiabloPhrase);
-            break;
-        } else {
-            System.out.println(getPositionPhrase);
-        }
     }
 
 
+    public static int diabloDefends(){
+        return getRandomNumber(3);
     }
 
     public static void main(String[] args) {
-      diabloPosition =  getRandomNumber(4);
-      findDiablo();
+        diabloPosition = getRandomNumber(4);
+
+        findDiablo();
+    }
+
+    public static void findDiablo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getFirstPositionPhrase);
+        while (true) {
+            int position = scanner.nextInt();
+            if (position == diabloPosition) {
+                System.out.println(findDiabloPhrase);
+                break;
+            } else {
+                System.out.println(getPositionPhrase);
+            }
+        }
     }
 
     public static int getRandomNumber(int range) {
