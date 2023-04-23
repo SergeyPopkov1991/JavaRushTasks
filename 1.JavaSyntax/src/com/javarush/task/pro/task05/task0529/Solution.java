d95faf1564f79b25660b8e3096912a10ee32ccb5
Sergey <wiggo91@mail.ru>
2023-04-23 19:56:40 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -31,31 +31,31 @@ public class Solution {
 
         }
 
-        for (int i = 0; i < bombs.length ; i++) {
-            for (int count = 0; count < 10 ; ) {
-                int j = (int) (Math.random() * width);
-                if(bombs[i][j] == 0 ){
-                    bombs[i][j] = 1;
-                    count++;
+
+        int countRobotanks = 10;
+        while (countRobotanks > 0) {
+            bombs = new int[height][width];
+            for (int i = 0; i < bombs.length; i++) {
+                for (int count = 0; count < 10; ) {
+                    int j = (int) (Math.random() * width);
+                    if (bombs[i][j] == 0) {
+                        bombs[i][j] = 1;
+                        count++;
+                    }
                 }
             }
-        }
 
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (field[i][j].equals(robotank) && bombs[i][j] == 1 ){
-                    field[i][j] = hit;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (field[i][j].equals(robotank) && bombs[i][j] == 1) {
+                        field[i][j] = hit;
+                        countRobotanks--;
+                    }
                 }
-
-
             }
 
         }
-
-
-
-
 
 
 
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0720/Nimrod.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0720;
+
+public class Nimrod {
+    public static int laser = 100000;
+    public static int rocket = 10000 ;
+    public static int angelicaKiss;
+
+    public int health = 1000000;
+
+    public void  defend(int damage){
+        health = health - damage;
+    }
+
+    public int attack() {
+        return rocket;
+    }
+}
