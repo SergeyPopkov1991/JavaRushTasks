b2d29d7f30cfcdf1f579fae8638e43f27ecc661b
Sergey <wiggo91@mail.ru>
2023-04-25 22:34:07 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0719/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0719;
+
+/* 
+Космическая Робосутра
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        AutomaticKitchen x =new AutomaticKitchen();
+        Gun v = new Gun();
+        ProtectiveFieldGenerator c = new ProtectiveFieldGenerator();
+        RepairDrone u = new RepairDrone();
+        RobotCleaner k = new RobotCleaner();
+    }
+}
