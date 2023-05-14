3f060ab7a6ba43f9419b6b55bb8e3751f40453f9
Sergey <wiggo91@mail.ru>
2023-05-14 19:39:49 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0904/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0904/Solution.java
@@ -5,10 +5,10 @@ package com.javarush.task.pro.task09.task0904;
 */
 
 public class Solution {
-    public static char first = 'u004A';
-    public static char second = a;
-    public static char third = "\u0076";
-    public static char fourth = {a};
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
 
     public static void main(String[] args) {
         System.out.print(first);
