eccc2be99eb195975d69759eace348406e441573
Sergey <wiggo91@mail.ru>
2023-05-17 22:32:11 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1001;
+
+/* 
+Создадим небоскреб
+*/
+
+public class Skyscraper {
+
+    //напишите тут ваш код
+    public Skyscraper() {
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+
+
+    }
+}
