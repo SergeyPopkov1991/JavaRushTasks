c07229596b7d6091fe77f24e51b8411d5f32667c
Sergey <wiggo91@mail.ru>
2023-05-18 22:39:42 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1025/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1025;
+
+/* 
+Взлом ретранслятора
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Repeater repeater = new Repeater();
+
+        repeater.setFrequency(300);
+        repeater.setTrajectory("elliptic");
+
+    }
+}
