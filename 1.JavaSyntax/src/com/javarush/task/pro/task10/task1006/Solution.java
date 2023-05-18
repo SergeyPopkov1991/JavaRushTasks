08f77f851f42f8d040c554649e04b53864b839bc
Sergey <wiggo91@mail.ru>
2023-05-18 22:14:02 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        HybridCar hybridCar = new HybridCar();
+        GasCar gasCar = new GasCar();
+        ElectricCar electricCar = new ElectricCar();
+    }
+}
