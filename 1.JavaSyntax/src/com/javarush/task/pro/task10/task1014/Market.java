ae9860168324015158322a8b7764de173f69bae8
Sergey <wiggo91@mail.ru>
2023-06-07 22:50:36 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1014/Market.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1014/Market.java
@@ -13,7 +13,7 @@ public class Market {
     }
 
     public static void makePurchases(boolean hasEggs) {
-        if (Market.hasEggs) {
+        if (hasEggs) {
             System.out.println("Купил 10 батонов");
         } else {
             System.out.println("Купил 1 батон");
