0cf6a91ac936f4b00fa9f4f41c901fb5b5269eb4
Sergey <wiggo91@mail.ru>
2023-06-12 13:55:00 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1107/Car.java
@@ -5,6 +5,20 @@ package com.javarush.task.pro.task11.task1107;
 */
 
 public class Car {
-    //напишите тут ваш код
+    Engine engine = new Engine();
+
+
+    class Engine{
+        private boolean isRunning ;
+
+        public void start() {
+            isRunning = true;
+        }
+
+        public void stop() {
+            isRunning = false;
+        }
+
+    }
 
 }
