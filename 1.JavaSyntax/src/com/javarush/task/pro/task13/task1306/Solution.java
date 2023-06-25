7b43e855f465ce472381c56c43293732a732c253
Sergey <wiggo91@mail.ru>
2023-06-25 13:31:00 +0500

    add Solution  JavaSyntax task

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1306/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1306/Solution.java
@@ -10,26 +10,15 @@ import java.util.Collections;
 public class Solution {
 
     public static void copy(ArrayList<String> destination, ArrayList<String> source) {
-        if(destination.size() < source.size()) {
-            throw new IndexOutOfBoundsException("Source does not fit in dest");
-        }
-        for (int i = 0; i < source.size(); i++) {
-            destination.set(i, source.get(i));
-        }
+        Collections.copy(destination , source);
+
     }
 
     public static void addAll(ArrayList<String> list, String... strings) {
-        for (String string : strings) {
-            list.add(string);
-        }
+      Collections.addAll(list , strings);
     }
 
     public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
-        for (int i = 0; i < list.size(); i++) {
-            String string = list.get(i);
-            if(string.equals(oldValue)) {
-                list.set(i, newValue);
-            }
-        }
+        Collections.replaceAll(list , oldValue , newValue);
     }
 }
