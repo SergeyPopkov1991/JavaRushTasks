4c647e2b63ea6b114f7a43678d8a291b63c5d844
Sergey <wiggo91@mail.ru>
2023-07-17 22:21:44 +0500

    add Solution  JavaSyntax task

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task15.task1523;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+/* 
+Получение информации по API
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://httpbin.org/post");
+       URLConnection connection = url.openConnection();
+       connection.setDoOutput(true);
+       try (OutputStream outputStream = connection.getOutputStream(); PrintStream sender = new PrintStream(outputStream)) {
+
+        sender.println("Привет");
+       }
+
+       try (InputStream inputStream = connection.getInputStream() ; BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+        while (reader.ready()) {
+            System.out.println(reader.readLine());
+        }
+       }
+    }
+}
+
