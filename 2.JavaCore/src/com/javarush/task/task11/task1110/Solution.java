3285d24d18e49da5d0c866bd6cd336de5ef7ed3e
Sergey <wiggo91@mail.ru>
2023-07-27 21:15:00 +0500

    add Solution  JavaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task11.task1110;
+
+/* 
+Не забываем инкапсулировать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name = "nae";
+        private int age = 1;
+        private int weight = 1;
+        private int speed = 12;
+
+        private Cat(String name, int age, int weight) {
+
+        }
+
+        private String getName() {
+            return null;
+        }
+
+        private int getAge() {
+            return 0;
+        }
+
+        private void setWeight(int weight) {
+
+        }
+
+        private void setSpeed(int speed) {
+
+        }
+    }
+}
