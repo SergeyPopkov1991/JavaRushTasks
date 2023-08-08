3a94c2efbfd73e7fe5d3885a4d31961cb97a4303
Sergey <wiggo91@mail.ru>
2023-08-08 23:03:51 +0500

    add Solution  JavaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Computer.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task14.task1413;
+
+public class Computer {
+    private Keyboard keyboard;
+    private Mouse mouse;
+    private Monitor monitor;
+
+    public void setKeyboard(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Computer(Keyboard keyboard, Mouse mouse, Monitor monitor) {
+        this.keyboard = keyboard;
+        this.mouse = mouse;
+        this.monitor = monitor;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task14.task1414;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+MovieFactory
+*/
+
+public class Solution {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String key = null;
+        while ((key = bufferedReader.readLine()) != null){
+            Movie movie = MovieFactory.getMovie(key);
+            if (movie == null) {
+                return;
+            }
+            System.out.println(movie.getClass().getSimpleName());
+        }
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            }
+            if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+            if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+
+            //напишите тут ваш код, пункты 5,6
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
+}
