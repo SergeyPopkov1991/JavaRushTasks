3ea615ea8397e0efecba8a80749ff6de5955a6c5
Sergey <wiggo91@mail.ru>
2023-08-09 22:25:36 +0500

    add Solution  JavaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+НОД
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int v = scanner.nextInt();
+    }
+}
