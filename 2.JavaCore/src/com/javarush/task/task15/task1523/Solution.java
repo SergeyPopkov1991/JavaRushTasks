89eeef3f027f29caef55b7201536eeb23121568e
Sergey <wiggo91@mail.ru>
2023-08-16 22:57:46 +0500

    add Solution  JavaSyntax task

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    Solution(int i ) {
+
+    }
+
+    public Solution() {
+
+    }
+
+    private Solution(double i) {
+
+    }
+
+    protected Solution(long i ) {
+
+    }
+
+}
+
