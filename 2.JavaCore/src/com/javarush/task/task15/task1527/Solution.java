59731acf8c10d09961fe66baaaab7563f8f98971
Sergey <wiggo91@mail.ru>
2023-08-17 13:24:55 +0500

    add Solution  JavaSyntax task

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task15.task1527;
 
 import java.io.BufferedReader;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -12,7 +13,33 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine();
-        //напишите тут ваш код
+
+
+        String[] split = url.substring(url.lastIndexOf("?") + 1).split("&");
+        for (String str : split) {
+            if (str.contains("=")) {
+                System.out.print(str.substring(0, str.indexOf("=")) + " ");
+            } else {
+                System.out.print(str + " ");
+            }
+        }
+        System.out.println();
+
+        for (String str : split) {
+            if (str.contains("obj")) {
+                int index = str.indexOf("=");
+                String substring = str.substring(index + 1);
+                try {
+                    double parseDouble = Double.parseDouble(substring);
+                    alert(parseDouble);
+
+                } catch (Exception e) {
+                    alert(substring);
+                }
+
+            }
+
+        }
     }
 
     public static void alert(double value) {
