68cc6e91ce959bf7ddc958637482400d0157b141
Sergey <wiggo91@mail.ru>
2023-09-18 21:21:12 +0500

    add Solution  JavaCore task

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task18.task1809;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/* 
+Реверс файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
