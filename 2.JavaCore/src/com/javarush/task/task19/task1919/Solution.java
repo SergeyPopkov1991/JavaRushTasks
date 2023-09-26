5bc67678b722da679c1705af390379cd242ea240
Sergey <wiggo91@mail.ru>
2023-09-26 22:14:15 +0500

    add Solution  JavaCore task

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task19.task1919;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
+/* 
+Считаем зарплаты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        while (reader.ready()) {
+            String string = reader.readLine();
+        }
+    }
+}
