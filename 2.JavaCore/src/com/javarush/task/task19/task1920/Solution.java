8fba17171d1ea584f33f33f95f1b402afbfded47
Sergey <wiggo91@mail.ru>
2023-10-25 22:05:22 +0500

    add Solution  JavaCore task

--- 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -12,5 +12,47 @@ import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) {
+        TreeMap<String, Double> salary = new TreeMap<>();
+
+        String fileName = args[0];
+
+        try (BufferedReader rd = new BufferedReader(new FileReader(fileName))) {
+            String[] splitedLine;
+            String line;
+            Double currentValue;
+
+            while ((line = rd.readLine()) != null) {
+                splitedLine = line.split(" ");
+                String name = splitedLine[0];
+                double value = Double.parseDouble(splitedLine[1]);
+
+                if (salary.containsKey(name)) {
+                    currentValue = salary.get(name);
+                    salary.put(name, value + currentValue);
+                } else {
+                    salary.put(name, value);
+                }
+            }
+        } catch (IOException ignore) {
+            /*NOP */
+        }
+
+        double maxSalary = salary.firstEntry().getValue();
+        for (double value : salary.values()) {
+            if (value > maxSalary) {
+                maxSalary = value;
+            }
+        }
+
+        TreeSet<String> names = new TreeSet<>();
+        for (String name : salary.keySet()) {
+            if (maxSalary == salary.get(name)) {
+                names.add(name);
+            }
+        }
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
