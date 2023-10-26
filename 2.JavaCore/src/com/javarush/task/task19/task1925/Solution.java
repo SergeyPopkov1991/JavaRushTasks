13532b1c1551fbe2d6a71221efbd2267eea0f5df
Sergey <wiggo91@mail.ru>
2023-10-26 22:16:49 +0500

    add Solution  JavaCore task

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -12,6 +12,34 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
+        String fileName1 = args[0];
+        String fileName2 = args[1];
+        String input;
 
+        ArrayList<String> fileContent = new ArrayList<String>();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName1))) {
+            while ((input = fileReader.readLine()) != null)
+                fileContent.add(input);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+        StringBuffer sbLine = new StringBuffer();
+        for (String line : fileContent) {
+            String[] splitedLine = line.split(" ");
+            for (String word : splitedLine) {
+                if (word.length() > 6)
+
+                    sbLine.append(word).append(" ");
+            }
+        }
+
+        String resultLine = sbLine.toString().trim().replaceAll(" ", ",");
+        try (FileWriter fileWriter = new FileWriter(fileName2)) {
+            fileWriter.write(resultLine);
+        } catch (IOException ignore) {
+            /* NOP */
+        }
     }
 }
