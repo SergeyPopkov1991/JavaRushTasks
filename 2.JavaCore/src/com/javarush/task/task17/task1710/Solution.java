7c06521b285d90985c8b5cf9e654494d5db65569
Sergey <wiggo91@mail.ru>
2023-12-13 22:59:01 +0500

    add Solution  to javacore

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,7 +19,53 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
-        //напишите тут ваш код
+
+    public static void main(String[] args) throws ParseException {
+        Person person = null;
+        SimpleDateFormat input = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        SimpleDateFormat output = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        if (args[0].equals("-c")) {
+            if (args[2].equals("м")) {
+               person = Person.createMale(args[1] , input.parse(args[3]));
+
+            } else if (args[2].equals("ж")) {
+                person = Person.createFemale(args[1] , input.parse(args[3]));
+            }
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+        } else if (args[0].equals("-r")) {
+            int id = Integer.parseInt(args[1]);
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                String sex = person.getSex().equals(Sex.MALE) ? "м" : "ж";
+                String format = output.format(person.getBirthDate());
+                System.out.println(person.getName() + " " + sex + " " + format);
+            }
+
+        } else if (args[0].equals("-u")) {
+            int id = Integer.parseInt(args[1]);
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                person.setName(args[2]);
+                if (args[3].equals("м")){
+                    person.setSex(Sex.MALE);
+                } else if (args[3].equals("ж")) {
+                    person.setSex(Sex.FEMALE);
+                }
+                person.setBirthDate(input.parse(args[4]));
+            }
+
+        } else if (args[0].equals("-d")) {
+            int id = Integer.parseInt(args[1]);
+            if (id >= 0 && id < allPeople.size()) {
+                person = allPeople.get(id);
+                person.setName(null);
+                person.setSex(null);
+                person.setBirthDate(null);
+            }
+
+        }
+
+
     }
 }
