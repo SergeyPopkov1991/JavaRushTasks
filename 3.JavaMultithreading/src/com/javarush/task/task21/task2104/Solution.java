f2f04979720531fb937fbf3be73be04aca0272b3
Sergey <wiggo91@mail.ru>
2023-12-18 21:58:59 +0500

    add Solution  to Multithreading

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2104;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -15,12 +16,19 @@ public class Solution {
         this.last = last;
     }
 
-    public boolean equals(Solution n) {
-        return n.first.equals(first) && n.last.equals(last);
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!(object instanceof Solution)) return false;
+        Solution solution = (Solution) object;
+        return Objects.equals(first, solution.first) && Objects.equals(last, solution.last);
     }
 
+    @Override
     public int hashCode() {
-        return 31 * first.hashCode() + last.hashCode();
+        return Objects.hash(first, last);
     }
 
     public static void main(String[] args) {
