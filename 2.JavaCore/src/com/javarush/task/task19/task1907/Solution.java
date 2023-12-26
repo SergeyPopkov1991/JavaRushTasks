18ca1a6c8f8e714f437af9f024c10c2dbf6845aa
Sergey <wiggo91@mail.ru>
2023-12-26 19:45:19 +0500

    add Solution  to Multithreading

--- 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /* 
 Считаем слово
@@ -11,20 +15,23 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        try(BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader reader = new BufferedReader(new FileReader(bfr.readLine()))) {
-            int count = 0;
-            while (reader.ready()) {
-                String string = reader.readLine();
-                String replace = string.replaceAll("\\p{P}", " ");
-                String[] strings = replace.split(" ");
-                for (String str : strings) {
-                    if (str.equals("world")) count++;
+//        try(BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader reader = new BufferedReader(new FileReader(bfr.readLine()))) {
+//            int count = 0;
+//            while (reader.ready()) {
+//                String string = reader.readLine();
+//                String replace = string.replaceAll("\\p{P}", " ");
+//                String[] strings = replace.split(" ");
+//                for (String str : strings) {
+//                    if (str.equals("world")) count++;
+//
+//                }
+//            }
+//            System.out.println(count);
+//
+//        }
+        System.out.println(Arrays.stream(Files.readString(Paths.get(new Scanner(System.in).nextLine())).replaceAll("\\p{P}", " ").split(" "))
+                .filter(str -> str.equals("world")).count());
 
-                }
-            }
-            System.out.println(count);
-
-        }
     }
 }
--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task19.task1919;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 import java.util.TreeMap;
 
 /* 
@@ -10,6 +12,26 @@ import java.util.TreeMap;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(args[0]))) {
+            Map<String, Double> map = new TreeMap<>();
+            while (bfr.ready()) {
+                String line = bfr.readLine();
+                String[] split = line.split(" ");
+                String key = split[0];
+                Double value = Double.parseDouble(split[1]);
+                if (map.containsKey(key)) {
+                    double newValue = map.get(key) + value;
+                    map.put(key, newValue);
+                } else {
+                    map.put(key, value);
+
+                }
+//                map.merge(key,value,Double::sum);
+            }
+            for (Map.Entry<String, Double> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        }
     }
 }
