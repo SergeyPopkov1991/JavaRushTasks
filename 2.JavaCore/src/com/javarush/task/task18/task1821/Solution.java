9b6876f43872ac8047cca88b93964a3da39bd145
Sergey <wiggo91@mail.ru>
2024-01-06 17:43:38 +0500

    add Solution  to JavaCore

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -1,14 +1,39 @@
 package com.javarush.task.task18.task1821;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /* 
 Встречаемость символов
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+//        Map<Character , Integer> map = new TreeMap<>();
+//        try (FileReader reader = new FileReader(args[0])) {
+//            while (reader.ready()) {
+//                char read =(char) reader.read();
+//                if (!map.containsKey(read)) {
+//                    map.put(read , 1);
+//
+//                } else {
+//                    map.put(read , map.get(read) + 1);
+//                }
+//            }
+//        }
+//        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
+        Files.readString(Paths.get(args[0])).chars().boxed()
+                .collect(Collectors.toMap(aChar -> (char) (aChar.intValue()), aChar -> 1, Integer::sum, TreeMap::new))
+                .forEach((key, value) -> System.out.println(key + " " + value));
 
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task18.task1822;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+/* 
+Поиск данных внутри файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader(console.readLine()))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String[] split = line.split(" ");
+                if (split[0].equals(args[0])) {
+                    System.out.println(line);
+                }
+            }
+        }
+
+
+
+    }
+}
