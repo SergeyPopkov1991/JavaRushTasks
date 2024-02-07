20dc2751e842d68a1e8d6871d82a7f508cf7217c
Sergey <wiggo91@mail.ru>
2024-02-07 22:41:21 +0500

    add Solution  to Multithreading

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -0,0 +1,49 @@
+package com.javarush.task.task24.task2413;
+
+public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    abstract void draw(Canvas canvas);
+    abstract void move();
+
+    boolean intersects(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
+
+}
