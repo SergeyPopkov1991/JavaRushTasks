c220b145ea16be2890b475610eb9555732bf66cb
Sergey <wiggo91@mail.ru>
2024-03-27 22:41:52 +0500

    add Solution  to Multithreading

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task29.task2909.human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
+
+    private List<Student> students = new ArrayList<>();
+    private String name;
+    private int age;
+
+    public University(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student getStudentWithAverageGrade() {
+        //TODO:
+        return null;
+    }
+
+    public Student getStudentWithMaxAverageGrade(double averageGrade) {
+        //TODO:
+        return null;
+    }
+
+    public void getStudentWithMinAverageGradeAndExpel() {
+        //TODO:
+    }
+}
