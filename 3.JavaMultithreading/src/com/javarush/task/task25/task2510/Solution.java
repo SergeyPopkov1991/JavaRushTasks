0c6b50f837353c2bebed96e92ad420a2c987189e
Sergey <wiggo91@mail.ru>
2024-04-02 22:25:01 +0500

    add Solution  to Multithreading

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -7,6 +7,17 @@ package com.javarush.task.task25.task2510;
 public class Solution extends Thread {
 
     public Solution() {
+        this.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                String message = null;
+                if (e instanceof Error) message = "Нельзя дальше работать";
+                else if (e instanceof Exception) message = "Надо обработать";
+                else if (e instanceof Throwable) message = "Поживем - увидим";
+                System.out.println(message);
+
+            }
+        });
     }
 
     public static void main(String[] args) {
