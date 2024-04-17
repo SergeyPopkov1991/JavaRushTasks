3f98c198ce748ea295625d16e5960b77b2ea8365
Sergey <wiggo91@mail.ru>
2024-04-17 22:53:21 +0500

    add Solution  to Multithreading

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,56 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message)  {
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Сообщение не отправлено");
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        
+        ConsoleHelper.writeMessage("Введите порт сервера");
+        int port = ConsoleHelper.readInt();
+        try( ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске сервера");
+        }
+
+
+    }
+
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
