af8c757da6cb1270d65d30d5fe886d721e5a8d40
Sergey <wiggo91@mail.ru>
2024-04-21 21:14:03 +0500

    add Solution  to Multithreading

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-    public static void sendBroadcastMessage(Message message)  {
+    public static void sendBroadcastMessage(Message message) {
         for (Connection connection : connectionMap.values()) {
             try {
                 connection.send(message);
@@ -23,7 +24,7 @@ public class Server {
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера");
         int port = ConsoleHelper.readInt();
-        try( ServerSocket serverSocket = new ServerSocket(port)) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             ConsoleHelper.writeMessage("Сервер запущен");
             while (true) {
                 Socket socket = serverSocket.accept();
@@ -46,11 +47,50 @@ public class Server {
         }
 
 
-
-
         @Override
         public void run() {
             super.run();
         }
+
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+
+            connection.send(new Message(MessageType.NAME_REQUEST, "Пожалуйста введите ваше имя."));
+            Message message = connection.receive();
+            String name = message.getData();
+            if (message.getType().equals(MessageType.USER_NAME)
+                    && Objects.nonNull(name)
+                    && !name.isEmpty()
+                    && !connectionMap.containsKey(name)) {
+                connectionMap.put(name, connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED, "Имя было принято"));
+                return name;
+
+            } else {
+                ConsoleHelper.writeMessage("Введите запрос сноваю");
+                return serverHandshake(connection);
+            }
+
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException {
+            for (String name : connectionMap.keySet()) {
+               if (!name.equals(userName)) {
+                   connection.send(new Message(MessageType.USER_ADDED , name));
+               }
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType()==(MessageType.TEXT)) {
+                    sendBroadcastMessage(new Message(MessageType.TEXT , userName+ ": " + message.getData()));
+                } else {
+                    ConsoleHelper.writeMessage("Ошибка");
+                }
+            }
+        }
+
     }
 }
