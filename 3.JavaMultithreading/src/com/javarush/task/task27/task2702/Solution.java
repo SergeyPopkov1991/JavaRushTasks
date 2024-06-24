a69f411fb7975fba96d3fe2f2e08b9838f8aa320
Sergey <wiggo91@mail.ru>
2024-06-24 22:45:34 +0500

    add Solution  to Multithreading

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
@@ -10,10 +10,19 @@ public class Solution {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j > i) {
+
                     System.out.println();
+                    continue label;
+
                 }
                 System.out.print("S");
+
+
+
+
             }
+
         }
+
     }
 }
